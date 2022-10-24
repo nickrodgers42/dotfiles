c83072e76e17cea9fc45fd5a11cb02fa1b428e38
Nick Rodgers <nick.rodgers42@gmail.com>
2022-10-24 09:57:07 -0400

    Fix sumneko vim diagnostics

--- vim/init.lua
+++ vim/init.lua
@@ -366,3 +366,14 @@ for _, name in ipairs(servers) do
     capabilities = capabilities
   }
 end
+lspconfig.sumneko_lua.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+}
