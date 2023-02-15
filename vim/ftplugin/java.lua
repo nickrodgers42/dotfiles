d84bbca01f050e6960fa49d951d988ddfabaae94
Nick Rodgers <nick.rodgers42@gmail.com>
2023-02-15 12:25:04 -0500

    Update java ftplugin

--- vim/ftplugin/java.lua
+++ vim/ftplugin/java.lua
@@ -8,8 +8,11 @@ end
 
 local function get_os()
     local handle = io.popen("uname")
-    local uname = handle:read("*a")
-    handle:close()
+    local uname = ""
+    if handle ~= nil then
+        uname = handle:read("*a")
+        handle:close()
+    end
     uname = trim(uname)
     if (uname == "Darwin") then
         return "mac"
@@ -22,13 +25,14 @@ local function get_equinox_version(plugins_dir)
     local cmd = "ls " .. plugins_dir .. " | "
     cmd = cmd .. "grep equinox.launcher_"
     local handle = io.popen(cmd)
-    local equinox_version = handle:read("*a")
-    handle:close()
+    local equinox_version = ""
+    if handle ~= nil then
+        equinox_version = handle:read("*a")
+        handle:close()
+    end
     return trim(equinox_version)
 end
 
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = home .. '/.java-workspace/' .. project_name
 local plugins_dir = jdtls_dir .. "plugins/"
 local jar = plugins_dir .. get_equinox_version(plugins_dir)
 
@@ -55,6 +59,17 @@ map('n', '<Leader>dT', [[<Cmd>lua require'jdtls'.test_class()<CR>]], { noremap =
 map('n', '<Leader>dt', [[<Cmd>lua require'jdtls'.test_nearest_method()<CR>]], { noremap = true })
 map('n', '<Leader>ds', [[<Cmd>lua require('jdtls.dap').setup_dap_main_class_configs()<CR>]], { noremap = true })
 
+-- local root_dir = require("jdtls.setup").find_root({"packageInfo"}, "Config")
+package.path = package.path .. ";" .. home .. "/.work.lua"
+local root_dir = require("work").find_root_work()
+local ws_folders_jdtls = {}
+if root_dir then
+    ws_folders_jdtls = require("work").add_ws_root_folders(root_dir)
+else
+    root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'})
+end
+local eclipse_workspace = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
 local config = {
     on_attach = on_attach,
     cmd = {
@@ -70,14 +85,16 @@ local config = {
         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
         '-jar', jar,
         '-configuration', jdtls_dir .. "/config_" .. get_os(),
-        '-data', workspace_dir
+        '--jvm-arg=-javaagent:' .. home .. "/Developer/lombok.jar",
+        '-data', eclipse_workspace
     },
-    root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+    root_dir = root_dir,
     settings = {
         java = {}
     },
     init_options = {
-        bundles = bundles
+        bundles = bundles,
+        workspace_folders = ws_folders_jdtls
     },
 }
 require('jdtls').start_or_attach(config)
