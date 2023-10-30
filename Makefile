08bd6f05c6f0322e63ead10f245e9afc4ca89336
Nick Rodgers <nick.rodgers42@gmail.com>
2023-10-30 14:07:36 -0400

    Install dependencies from script

--- Makefile
+++ Makefile
@@ -2,47 +2,6 @@ NVIM_HOME := $(shell echo $${XDG_DATA_HOME:-$$HOME/.local/share})
 INIT_DIR := $(HOME)/.config/nvim
 
 
-brewinstall:
-	/bin/bash -c "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-
-brewdeps:
-	brew install \
-		bat \
-		exa \
-		fd \
-		fzf \
-		gcc \
-		make \
-		neovim \
-		ripgrep \
-		stow \
-		tmux
-
-
-brewenv:
-	(echo; echo 'eval "$$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/runner/.bashrc
-	eval "$$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-
-nvm:
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-
-
-node:
-	nvm install node
-
-
-nodedeps:
-	npm i -g \
-		eslint \
-		tldr
-
-
-tpm:
-	-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-
 vim:
 	stow -t ~ --ignore=ftplugin vim
 	mkdir -p $(INIT_DIR)
@@ -51,14 +10,10 @@ vim:
 
 tmux-conf:
 	stow -t ~ tmux
-	tmux start-server
-	tmux new-session -d
-	~/.tmux/plugins/tpm/bin/install_plugins
-	tmux kill-server
 
 
 gitconfig:
-	stow -t ~ git
+	stow --adopt -t ~ git
 
 
 intellij:
--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 install_brew() {
     echo "Installing brew..."
-	/bin/bash -c "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 install_brew_deps() {
     echo "Installing dependencies from brew..."
     brew install \
         bat \
-        exa \
+        eza \
         fd \
         fzf \
         gcc \
@@ -20,17 +20,47 @@ install_brew_deps() {
         tmux
 }
 
+install_nvm() {
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+}
+
+install_node() {
+    nvm install node
+}
+
+install_node_deps() {
+	npm i -g \
+		eslint \
+		tldr
+}
+
+install_tpm() {
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
+
+stow_dotfiles() {
+    make stowall
+}
+
 install() {
     local OS=$(uname -s)
     
     install_brew
-    if [[ $OS eq "Linux" ]]
+    if [[ $OS = "Linux" ]]
     then
-        (echo; echo 'eval "$$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/runner/.bashrc
-        eval "$$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/runner/.bashrc
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
     fi
 
     install_brew_deps
+    install_nvm
+    install_node
+    install_node_deps
+    install_tpm
+    stow_dotfiles
 }
 
 install
